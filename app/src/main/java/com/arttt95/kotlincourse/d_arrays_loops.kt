3581be61954369a0631255183aef537d13a192f6
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-12-05 10:43:41 -0300

    05DEZ24 - Aula 81 AS - Arrays & Loops: While e For ('withIndex()').

---
+++ app/src/main/java/com/arttt95/kotlincourse/d_arrays_loops.kt
@@ -0,0 +1,32 @@
+package com.arttt95.kotlincourse
+
+fun main() {
+
+    /*val nomes = arrayOf(
+        "Penaldo", "Pessi"
+    )
+
+    nomes[1] = "Menino Ney"
+
+    println(nomes[1])*/
+
+    /*var num = 1
+
+    while(num <= 10) {
+        println("Executando $num vez")
+        num++
+    }*/
+
+    /*for(num in 1..10) {
+        println("Executando: $num")
+    }*/
+
+    val nomes = arrayOf(
+        "Penaldo", "Pessi", "Menino Ney", "Shevchenko", "Nedved"
+    )
+
+    for( (i, nome) in nomes.withIndex()) {
+        println("$i: $nome")
+    }
+
+}
