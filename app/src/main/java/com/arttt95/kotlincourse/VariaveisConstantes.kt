e28ce4093dc4055e14adf6f15a9b28f343489b9f
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-12-04 22:25:07 -0300

    04DEZ24 - Aula 78 AS - Variáveis e Constantes.

---
+++ app/src/main/java/com/arttt95/kotlincourse/VariaveisConstantes.kt
@@ -0,0 +1,36 @@
+package com.arttt95.kotlincourse
+
+fun retornaNomeUsuario() {
+    // Executa e retorna alguma coisa
+}
+
+class Usuario {
+
+}
+
+const val ninja2 = "Obito" // Imutável -> Compile time
+
+fun main() {
+
+    var nome = "Cristiano Penaldo" // Mutável
+
+    nome = "Lionel Pessi"
+
+    val ninja: String = "Itachi" // Imutável -> Aceita somente Strings -> Runtime
+
+    val nome3 = retornaNomeUsuario()
+
+    val num1 = 20
+    val num2 = 30
+    val res = num1 + num2
+
+    var num: Int = 10
+
+    val usuario = Usuario()
+
+    println(nome)
+    println(ninja)
+    println(res)
+
+
+}
