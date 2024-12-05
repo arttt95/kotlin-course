be01285921e4a8195d11df7b11fcd95971444492
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-12-05 11:05:35 -0300

    05DEZ24 - Aula 84 AS - Functions: return/no return.

---
+++ app/src/main/java/com/arttt95/kotlincourse/e_func_return.kt
@@ -0,0 +1,14 @@
+package com.arttt95.kotlincourse
+
+// COM return & SEM return
+fun somar(): Int {
+//    println(5 + 5)
+    return 5 + 5
+}
+
+fun main() {
+
+    val soma = somar()
+    println(soma)
+
+}
