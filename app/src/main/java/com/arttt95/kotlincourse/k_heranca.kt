cd37be269d38160930c8f44206938f89dbcd7c12
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-12-05 19:02:36 -0300

    05DEZ24 - Aula 90 As - Herança.

---
+++ app/src/main/java/com/arttt95/kotlincourse/k_heranca.kt
@@ -0,0 +1,66 @@
+package com.arttt95.kotlincourse
+
+open class Animal constructor(
+    private var nome: String = "",
+    private var cor: String = "",
+    private var tamanho: Int = 0, // 1 -> P, 2 -> M, 3 -> G
+    private var peso: Double = 0.0
+) { // Classe Pai ou Superclasse
+
+    init {
+//        println("Objeto iniciado")
+    }
+
+    open fun dormir() = println("O animal: $nome está dormindo...") // Inline
+
+    open fun correr() = println("Correndo como um ") // Inline
+
+}
+
+class Cachorro: Animal() { // Subclasse ou Classe Filha
+    public fun latir() = println("O cachorro está latindo...")
+
+    override fun correr() {
+        super.correr()
+        println("cachorro com 4 patas")
+    }
+}
+
+class Passaro: Animal() { // Subclasse ou Classe Filha
+    public fun voar() = println("O passaro está voando...")
+
+    override fun correr() {
+        super.correr()
+        println("passáro com 2 pernas")
+    }
+}
+
+fun main() {
+
+    /*val animal = Animal("Snoop", "Marrom", 2, 580.5)
+
+    animal.dormir()
+    animal.correr()*/
+
+    val dog = Cachorro()
+    dog.latir()
+    dog.correr()
+
+    println("\n")
+
+    val bird = Passaro()
+    bird.voar()
+    bird.correr()
+
+
+}
+
+
+
+
+
+
+
+
+
+
