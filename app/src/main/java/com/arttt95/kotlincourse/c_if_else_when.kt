d60a896d94cbb1003a9418c64e5b4585c6162058
Arthur Damasceno <thuurdamasceno@gmail.com>
2024-12-05 09:56:15 -0300

    05DEZ24 - Aula 81 AS - If, Else & When.

---
+++ app/src/main/java/com/arttt95/kotlincourse/c_if_else_when.kt
@@ -0,0 +1,40 @@
+package com.arttt95.kotlincourse
+
+fun main() {
+
+    /*val nota = 5
+    val condicao = nota >= 6*/
+
+    /*val opcao = 5
+
+    if (opcao == 1){
+        println("Cartão de Crédito")
+    } else if (opcao == 2) {
+        println("Extrato Bancário")
+    } else if (opcao == 3){
+        println("Cancelamentos")
+    } else {
+        println("Nenhuma opcao selecionada")
+    }*/
+
+    /*val opcao = 6
+
+    if (opcao in 1..3) {
+        println("Opções selecionadas: 1, 2 e 3.")
+    } else if (opcao in 4..6) {
+        println("Opções selecionadas: 4, 5 e 6.")
+    } else {
+        println("Nenhuma opção selecionada.")
+    }*/
+
+    val valor = 7
+
+    when (valor){
+        1 -> println("Cartão de crédito")
+        2 -> println("Extrato bancário")
+        3 -> println("Cancelamentos")
+        in 4..7 -> println("Saldo")
+        else -> println("Nenhuma opção selecionada")
+    }
+
+}
